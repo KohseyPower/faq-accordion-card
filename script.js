54d1f94f1e9c11d0fe9250297a73a392db23df8c
Kohsey <kohsey.dufour@gmail.com>
2022-07-10 22:06:18 +0200

    arrows animation

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 function showUp(event) {
-    let para = event.target.parentNode.nextSibling;
-    console.log(para.hidden === true);
-    if (para.hidden === true) {
-        para.setAttribute("hidden", "false");
-    } else {
-        para.hidden = true;
+    /* Initialization*/
+    /* enable hidden to every paragraph */
+    let parent = document.querySelector('main .faq');
+    let slctAll = parent.querySelectorAll('p');
+    let slctAllArrows = parent.querySelectorAll('img');
+    console.log(slctAllArrows);
+    for (let i = 0; i < slctAll.length; i++) {
+        slctAll[i].hidden = true;
+        slctAllArrows[i].style.transform = "rotate(0)";
     };
+
+    /* selected para can be hiden or "unhidden" */
+    let div = event.target.parentNode;
+    let para = next(div);
+    let arrow = next(event.target);
+
+    if (para.hidden === true) {
+        para.hidden = false;
+        arrow.style.transform = "rotate(180deg)";
+    };
+
+};
+
+/* function to get the hidden nextSibling*/
+function next(elem) {
+    do {
+        elem = elem.nextSibling;
+    } while (elem && elem.nodeType !== 1);
+    return elem;
+}
