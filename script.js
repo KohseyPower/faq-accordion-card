6629767f3a8a14cbe2524c313f4abf03dee745f2
Kohsey <kohsey.dufour@gmail.com>
2022-07-14 15:02:01 +0200

    finale solution for desktop

--- script.js
+++ script.js
@@ -3,21 +3,27 @@ function showUp(event) {
     /* enable hidden to every paragraph */
     let parent = document.querySelector('main .faq');
     let slctAll = parent.querySelectorAll('p');
+
     let slctAllArrows = parent.querySelectorAll('img');
-    console.log(slctAllArrows);
+
+    let slctAllBorder = parent.querySelectorAll('section div');
+
     for (let i = 0; i < slctAll.length; i++) {
         slctAll[i].hidden = true;
         slctAllArrows[i].style.transform = "rotate(0)";
+        slctAllBorder[i].style.borderBottom = "1px solid var(--Light-grayish-blue)";
     };
 
     /* selected para can be hiden or "unhidden" */
     let div = event.target.parentNode;
     let para = next(div);
     let arrow = next(event.target);
+    let border = event.target.parentNode;
 
     if (para.hidden === true) {
         para.hidden = false;
         arrow.style.transform = "rotate(180deg)";
+        border.style.borderBottom = "none";
     };
 
 };
